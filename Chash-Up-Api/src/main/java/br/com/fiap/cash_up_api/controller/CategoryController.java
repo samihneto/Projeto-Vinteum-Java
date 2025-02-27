4579b1494919e390d88577d03fa7a2aefbdc4376
Samir Neto <samihneto@gmail.com>
2025-02-27 09:36:13 -0300

    "category: create, get"

--- Chash-Up-Api/src/main/java/br/com/fiap/cash_up_api/controller/CategoryController.java
+++ Chash-Up-Api/src/main/java/br/com/fiap/cash_up_api/controller/CategoryController.java
@@ -1,10 +1,17 @@
 package br.com.fiap.cash_up_api.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.cash_up_api.model.Category;
@@ -12,18 +19,35 @@ import br.com.fiap.cash_up_api.model.Category;
 @RestController // component
 public class CategoryController {
 
+    private List<Category> repository = new ArrayList<>();
+
     // listar todas as categorias
     // GET :8080/categories -> json
     @GetMapping("/categories")
     public List<Category> index(){ //mocky
-        return List.of(
-            new Category(1L, "Educação", "book"),
-            new Category(2L, "Lazer", "casino"),
-            new Category(3L, "Salário", "payments")
-        );
+            return repository;
     }
 
     // cadastrar categoria
+    @PostMapping("/categories")
+    // @ResponseStatus(code = HttpStatus.CREATED)
+    public ResponseEntity<Category> create(@RequestBody Category category) {
+        System.out.println("Cadastrando categoria" + category.getName());
+        repository.add(category);
+        return ResponseEntity.status(201).body(category);
+    }
+
+    // retornar uma categoria
+    @GetMapping("/categories/{id}")
+    public ResponseEntity<Category> get(@PathVariable Long id) {
+        System.out.println("Buscando categoria" + id);
+        var category = repository.stream().filter(c -> c.getId().equals(id)).findFirst();
+        if (category.isEmpty()) {
+            return ResponseEntity.status(404).build();
+        }
+        // return ResponseEntity.status(200).body(category.get());
+        return ResponseEntity.ok(category.get());
+    }
 
     // apagar categoria
 
